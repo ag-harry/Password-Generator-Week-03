a928f3941a3f3062634791efeba440bb4c1a6eda
ag-harry <ag.harry11@gmail.com>
2023-02-28 19:32:51 -0800

    Password_generator

---
+++ Develop/script.js
@@ -0,0 +1,15 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function genPassword() {
+  var chars= "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+~`";
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
